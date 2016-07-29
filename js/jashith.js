02ee64101e089c033e63a424ecf47c31c17c1cfb
Vijayakumar Moorthy <VijayakumarMoorthy@users.noreply.github.com>
2016-07-29 12:20:06 +0530

    Add files via upload

--- js/jashith.js
+++ js/jashith.js
@@ -1,1 +1,42 @@
+console.log("jashith java script loaded");
 
+function isDefined(a)
+{
+    return (typeof(a)=="undefined" ? false : true);
+}
+try{
+	var JCL_timer = (function(){
+		this.serachVelocity = 300;
+		this.searchTrigger = '';
+		this.searchQuery = '';
+		this.callBack = function(obj){}
+		this.setDelayTime = function(velocity){
+			this.serachVelocity = velocity;
+		}
+		this.setCallBack = function(callback){
+			this.callback = callback;
+		}
+		this.fireCallBack = function(){
+			this.callback(this.searchQuery);
+		}
+		this.update = function(obj){
+			this.searchQuery = obj;
+			var th = this;
+			if(this.searchTrigger!=''){
+				clearTimeout(this.searchTrigger);
+				this.searchTrigger = '';
+			}
+
+			var velocity = obj.attr('data-delay');
+			//set custom velocity if avail
+			if(isDefined(velocity) && !isNaN(velocity))
+				this.setDelayTime(velocity);
+
+			th.searchTrigger = setTimeout(function(){
+				th.searchTrigger = '';
+				th.fireCallBack();
+			}, th.serachVelocity);
+		}
+		
+	});
+}catch(e) {console.log(e);}
